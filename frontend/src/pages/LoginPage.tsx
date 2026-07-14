207c88589b015a09187a53937a5ca9915c58eecd
Nattapat2550 <nyansungvon@gmail.com>
2026-07-14 10:14:17 +0700

    edit

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -32,7 +32,7 @@ export default function LoginPage() {
   };
 
   const handleGoogleLogin = () => {
-    window.location.href = 'https://gtymall.onrender.com/api/auth/google';
+    window.location.href = `${api.defaults.baseURL}/api/auth/google`;
   };
 
   return (
